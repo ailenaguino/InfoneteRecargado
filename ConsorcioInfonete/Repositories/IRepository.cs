129539178bba1d6f62335fa8f5cbdfbb1df47e8e
Tomas Garcia <tomas00garcia@gmail.com>
2020-11-01 17:10:18 -0300

    First commit

---
+++ ConsorcioInfonete/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Context
+{
+    interface IRepository<T>
+    {
+        void Alta(T m);
+
+        List<T> ObtenerTodos();
+
+        T ObtenerPorId(int id);
+
+        void Eliminar(int id);
+
+        void Modificar(T m);
+    }
+}
