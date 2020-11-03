c9fb9fc13309ac50a791b1b8420cc742ef5b76eb
Marcos Cabral <42642744+Marcos-Cabral@users.noreply.github.com>
2020-11-03 15:13:29 -0300

    listar y Alta de gasto

---
+++ ConsorcioInfonete/Services/GastoService.cs
@@ -0,0 +1,18 @@
+﻿using Repositories;
+using Repositories.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class GastoService:BaseService<GastoRepository,Gasto>
+    {
+        public GastoService(PW3_TP_20202CEntities ctx):base(ctx)
+        {
+
+        }
+    }
+}
