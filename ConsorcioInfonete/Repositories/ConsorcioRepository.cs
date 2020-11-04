1c2ee32327dddea2d67b5ce1f1fb0a36062a5efd
federodriguez98 <federicoj.rodriguez.98@gmail.com>
2020-11-04 14:24:00 -0300

    consorcio repository, service, model y controller

---
+++ ConsorcioInfonete/Repositories/ConsorcioRepository.cs
@@ -0,0 +1,21 @@
+﻿using Repositories.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class ConsorcioRepository : BaseRepository<Consorcio>
+    {
+        public ConsorcioRepository(PW3_TP_20202CEntities contexto) : base(contexto)
+        {
+        }
+
+        public override void Modificar(Consorcio m)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ConsorcioInfonete/Web/Controllers/ConsorcioController.cs
+++ ConsorcioInfonete/Web/Controllers/ConsorcioController.cs
@@ -1,15 +1,42 @@
-﻿using System;
+﻿using Repositories.Context;
+using Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Helpers;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class ConsorcioController : Controller
     {
-        // GET: Consorcio
+        private ConsorcioService consorcioService;
+        private UsuarioService usuarioService;
+
+        public ConsorcioController()
+        {
+            PW3_TP_20202CEntities contexto = new PW3_TP_20202CEntities();
+            consorcioService = new ConsorcioService(contexto);
+            usuarioService = new UsuarioService(contexto);
+        }
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Lista()
+        {
+            int idusuario = SessionHelper.GetCurrentSession().IdUsuario;
+            Usuario consorcios = usuarioService.ObtenerPorId(idusuario);
+            return View(consorcios);
+        }
+
+        
+
+        
+        public ActionResult Editar()
         {
             return View();
         }
