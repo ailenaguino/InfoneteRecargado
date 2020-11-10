993f67161433ad9e58a99d1c568d259bfa59498a
ailenaguino <ailenaguino@gmail.com>
2020-11-10 19:03:59 -0300

    Pongo en español las datatables y le agrego bootstrap a las vistas de Unidad

--- ConsorcioInfonete/Web/Scripts/datatable/pagination.js
+++ ConsorcioInfonete/Web/Scripts/datatable/pagination.js
@@ -3,6 +3,9 @@
         "paging": true,
          "ordering": false,
         "info": true,
-         "search": false
+        "search": false,
+         "language": {
+             "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
+        }
     });
 });
