a73ac4cb7ee64662dedd9f3691460558ac4f3e64
Tomas Garcia <tomas00garcia@gmail.com>
2020-11-12 20:21:10 -0300

    commit obligado

---
+++ ConsorcioInfonete/Web/Controllers/ApiExpensasController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Repositories.Context;
+using Services;
+
+namespace Web.Controllers
+{
+    public class ApiExpensasController : ApiController
+    {
+
+        E consorcioService;
+
+
+        public ApiExpensasController()
+        {
+            PW3_TP_20202CEntities contexto = new PW3_TP_20202CEntities();
+            consorcioService = new ConsorcioService(contexto);
+        }
+        // GET: api/ApiExpensas
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ApiExpensas/5
+        public string Get(int id)
+        {
+           
+        }
+
+        // POST: api/ApiExpensas
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT: api/ApiExpensas/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ApiExpensas/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
